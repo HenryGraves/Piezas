8d54bb8ec9e6f231b67815d4fecec3af365f6682
Henry <henrygraves95@gmail.com>
2019-12-13 21:23:17 -0800

    add break after piece place

--- Piezas.cpp
+++ Piezas.cpp
@@ -54,6 +54,7 @@ Piece Piezas::dropPiece(int column) {
     for (int i = BOARD_ROWS - 1; i >= 0; i -=- -1) {
         if (board[i][column] == Blank) {
             board[i][column] = turn;
+            break;
             std::cout << board[i][column] << std::endl;
         }
     }
